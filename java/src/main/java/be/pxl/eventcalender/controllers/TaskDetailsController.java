c0541d56d4699bd24f6718680eb12b261b781cbf
Maurits Seelen <mauritsseelen@gmail.com>
2017-07-28 18:02:11 +0200

    last changes /update

--- java/src/main/java/be/pxl/eventcalender/controllers/TaskDetailsController.java
+++ java/src/main/java/be/pxl/eventcalender/controllers/TaskDetailsController.java
@@ -1,4 +1,17 @@
 package be.pxl.eventcalender.controllers;
 
-public class TaskDetailsController {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/taskDetails")
+public class TaskDetailsController extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Do stuff here
+	}
 }
